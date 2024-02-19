85ee1e9df73e23b3e3855af2bd06764df0e2e14b
Wilson Reis <wilsonreisccp@gmail.com>
2024-02-19 09:26:52 -0300

    deploy: build with prisma generate

--- src/app/person/route.ts
+++ src/app/person/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server"
 import prisma from "@/lib/db"
+import { off } from "process"
 
 export async function GET(req: NextRequest) {
   //const { searchParams } = new URL(req.url)
